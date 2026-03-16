2841c97184e80127d244f10c1e38c1b1bc2d5b8e
emqnuele <emanuelee.faraci@gmail.com>
2026-03-16 19:16:52 +0100

    add LOG_LEVEL env var support

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,8 +1,12 @@
 import logging
+import os
 from rich.logging import RichHandler
 
 
 _loggers = {}
+
+# allow overriding log level without touching code
+_level = getattr(logging, os.getenv("LOG_LEVEL", "INFO").upper(), logging.INFO)
 
 
 def get_logger(name: str) -> logging.Logger:
@@ -10,12 +14,13 @@ def get_logger(name: str) -> logging.Logger:
         return _loggers[name]
 
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(_level)
     # avoid duplicate output with uvicorn
     logger.propagate = False
 
     if not logger.handlers:
         handler = RichHandler(rich_tracebacks=True, markup=False, show_path=False)
+        handler.setLevel(_level)
         logger.addHandler(handler)
 
     _loggers[name] = logger
